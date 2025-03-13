d0eebada70df92ded2057bd90b1590b11ae006a7
jednorukypilot <ondrejmarecek@gmail.com>
2025-03-13 23:05:50 +0100

    wip: responsive background

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,25 @@
 <script lang="ts">
 	import '../app.css';
+	import logo from '../../static/veterina_logo.svg';
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div id="bg-layout">
+	<div class="">
+		{@render children()}
+	</div>
+</div>
+
+<style>
+	#bg-layout {
+		height: 100%;
+		width: 100%;
+		min-height: 100vh;
+		background-color: #0b6e4d;
+		background-image: url('../../static/veterina_logo.svg');
+		background-size: contain;
+		background-repeat: no-repeat;
+		background-position: center;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-4xl font-bold text-center">Veterina Radotín</h1>
+<div class="grid grid-rows-2 md:grid-cols-2 gap-4 p-8">
+	<div class="bg-pink-200 h-380">01</div>
+
+	<div class=" bg-pink-200">02</div>
+</div>
